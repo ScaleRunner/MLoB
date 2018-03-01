6166d887c88323dbda1a01d28ec6ff2bce2a37a6
ScaleRunner <dennis-verheijden@live.nl>
2018-03-01 10:54:04 +0100

    Feature Extraction: Implemented training loop + framework for feature extraction

--- feature_extraction/model.py
+++ feature_extraction/model.py
@@ -1,9 +1,15 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import pandas as pd
+from feature_extraction.features import extract_features, train_test_split
 
-## TODO: load data
+# TODO: load data
 df = pd.DataFrame()
+features = extract_features(df)
+labels = df['labels']
+
+features_train, labels_train, features_test, labels_test = train_test_split(features, labels)
+
 n_samples = len(df.columns)
 n_features = len(df["features"]) # prob not
 n_outputs = len(df['labels'])
@@ -17,9 +23,13 @@ model = Sequential([
     Activation('softmax')  # Maybe, more research needed
 ])
 
-# Multiclass uses categorical crossentropy
+# Multiclass uses categorical cross-entropy
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-## TODO: Training Loop
+# Training
+model.fit(features_train, labels_train,
+          epochs=100,
+          batch_size=128)
+score = model.evaluate(features_test, labels_test, batch_size=128)
