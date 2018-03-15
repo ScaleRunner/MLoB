10f16cfa5578b7f779a50380d52e92e021b44794
Mick van Hulst <mick.vanhulst@gmail.com>
2018-03-15 13:27:32 +0100

    Added processed test dataset 1000 rows. Top 10% words. Fixed bug where cat where all zeroes was not included

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,13 +1,9 @@
 import pandas as pd
-import numpy as np
 
 # Text libs
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 from collections import Counter
-import pickle
-
-
 
 '''
 Get the top k percent of words for each class and represent those with a number
@@ -17,11 +13,20 @@ Other words get the <UNK> token
 
 def get_embedding_ids(data, top_perc, replacement_word):
     embedding_ids = []
-    for cat in ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']:
-        subset = data[data[cat] == 1]
-        cnt_words = Counter([x for l in subset['comment_vect_filtered'].values for x in l])
-        most_common_words = cnt_words.most_common(int(len(cnt_words.keys()) * top_perc))
-        embedding_ids.extend([x[0] for x in most_common_words])
+    for cat in ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate', 'Be_nice_bruh']:
+        if cat == 'Be_nice_bruh':
+            subset = data[(data['toxic'] == 0) & (data['severe_toxic'] == 0) & (data['obscene'] == 0)
+                & (data['threat'] == 0) & (data['insult'] == 0) & (data['identity_hate'] == 0)]
+            print(subset)
+        else:
+            subset = data[data[cat] == 1]
+
+        if len(subset) > 0:
+            flattened_list = [i for sublist in subset['comment_vect_filtered'].values for i in sublist]
+            cnt_words = Counter(flattened_list)
+            most_common_words = cnt_words.most_common(int(len(cnt_words) * top_perc))
+            embedding_ids.extend([x[0] for x in most_common_words])
+
 
     # Convert to set to get unique values, then back to list to support indexing.
     embedding_ids = list(set(embedding_ids))
@@ -41,16 +46,13 @@ Load size_to_load of the dataset,
 '''
 
 
-def main(size_to_load=100, top_perc=0.2):
+def main(size_to_load=None, top_perc=0.1):
     for value in ['train', 'test']:
         # Load data
         data = pd.read_csv('./data/{}.csv'.format(value), encoding='utf-8', nrows=size_to_load)
 
         # step 1: Remove NA values.
         data = data.dropna()
-
-        # TODO: Remove stopwords (custom list)
-        # cust_stopwords = ['the', 'a', 'an', ',']
 
         # Step 2: words to list
         tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
@@ -59,10 +61,6 @@ def main(size_to_load=100, top_perc=0.2):
         # Filter out stop words and convert the resulting set to lower case strings.
         stop = stopwords.words('english')
         data['comment_vect_filtered'] = data['comment_vect'].apply(lambda x: [i.lower() for i in x if i not in stop])
-
-        # Filter out stop words
-        stop = stopwords.words('english')
-        data['comment_vect_filtered'] = data['comment_vect_filtered'].apply(lambda x: [i for i in x if i not in stop])
 
         # If training, get embedding_ids
         if value == 'train':
